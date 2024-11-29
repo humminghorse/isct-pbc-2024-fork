bcfebc58ae388890e27b51709859feb9e3b17199
humminghorse <r.imahashi0724@gmail.com>
2024-11-29 22:37:12 +0900

    Exercise: API to get a list of images

--- learning-phase-2/src/app/api/images/route.ts
+++ learning-phase-2/src/app/api/images/route.ts
@@ -12,3 +12,18 @@ export async function POST(request: Request) {
 
   return NextResponse.json(response)
 }
+
+export async function GET() {
+  const response = await supabase.storage.from('learning-phase').list()
+  return NextResponse.json(response)
+}
+// Advanced: URLも取得する場合
+// export async function GET() {
+//   const response = await supabase.storage.from('learning-phase').list()
+//   const res: { name: string; url: string }[] = []
+//   response.data?.forEach((file) => {
+//     const { data } = supabase.storage.from('learning-phase').getPublicUrl(file.name)
+//     res.push({ name: file.name, url: data.publicUrl })
+//   })
+//   return NextResponse.json(res)
+// }
